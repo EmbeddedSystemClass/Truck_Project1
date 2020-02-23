634af4abeca5a832af017c3dc4295644cadc9ae1
Dan Hampleman <hwswhacker1256@gmail.com>
2020-02-23 06:37:58 -0700

    can't run bash script with cr/nl in linux

--- thread_io/test2.sh
+++ thread_io/test2.sh
@@ -1,18 +1,19 @@
+if [[ -z "${CROSS_COMPILE}" ]]; then
+  echo "run setARMpath.sh"
+  exit 1
+fi
 unzip -o ~/thread_io.zip
 mv mytypes.h ..
+make clean
 make &> out.txt
 if grep -q error out.txt
  then
- find2 error out.txt
- exit 1
+  find2 error out.txt
+  exit 1
 fi
-if grep undefined -q out.txt
- then
- find2 undefined out.txt
- exit 1
-fi
+#ftp 192.168.42.145
+echo "ssh'ing into 145"
+scp sched 192.168.0.145:/home/dan/dev/sched/sched2
+rm *.o
 ls -ltr sched
-scp sched 192.168.0.145:/home/dan/dev/sched
-mv sched sched2
-make clean
-mv sched2 sched
+mv sched ~
